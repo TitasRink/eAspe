efc49ebd9edf34f0153c031d33b7faf40960c6ce
Titas Rinkevicus <titas_rink@yahoo.com>
2023-01-27 17:56:15 +0200

    Sevices
    added few services category and items

---
+++ eAspeBusiness/Services/CategoryServices.cs
@@ -0,0 +1,49 @@
+﻿using eAspe.Models;
+using eAspeDatabase.SqlDbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace eAspeBusiness.Services
+{
+    public class CategoryServices
+    {
+        private readonly AspeContext _context;
+
+        public CategoryServices(AspeContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddNewCategoryAsync(Category categoryName)
+        {
+            await _context.Categories.AddAsync(categoryName);
+        }
+
+        public async Task RenameCategoryAsync(Category categoryName)
+        {
+            
+            var category = await _context.Categories.FirstOrDefaultAsync(c=>c.Id == categoryName.Id);
+            if (category != null)
+            {
+                category.Name = categoryName.Name;
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveNewCategoryAsync(Category categoryName)
+        {
+            var category = await _context.Categories.FirstOrDefaultAsync(c=>c.Name == categoryName.Name);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+            }
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Category>> GetCategoryListAsync()
+        {
+            var result = await _context.Categories.OrderBy(c => c.Id).ToListAsync();
+            return result;
+        }
+    }
+}
---
+++ eAspeBusiness/Services/ItemServices.cs
@@ -0,0 +1,65 @@
+﻿using eAspe.Models;
+using eAspeDatabase.SqlDbContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eAspeBusiness.Services
+{
+    public class ItemServices
+    {
+        private readonly AspeContext _context;
+
+        public ItemServices(AspeContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddNewItemAsync(Item item)
+        {
+            await _context.Items.AddAsync(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveItemAsync(Item item)
+        {
+            var result = await _context.Items.FirstOrDefaultAsync(c => c.Id == item.Id);
+            if (result != null)
+            {
+                _context.Items.Remove(result);
+            }
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RenameItemAsync(Item item)
+        {
+            var resultItem = await _context.Items.FirstOrDefaultAsync(c => c.Id == item.Id);
+            if (resultItem != null)
+            {
+                resultItem.Title = item.Title;
+                resultItem.ManufacturerId = item.ManufacturerId;
+                resultItem.Categories = item.Categories;
+                resultItem.Content = item.Content;
+                resultItem.ItemProperties = item.ItemProperties;
+                resultItem.Images = item.Images;
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<Item> GetItemById(Item item)
+        {
+            var itemsDetails = await _context.Items
+                .Include(c=>c.Categories)
+                .Include(m=>m.ManufacturerId)
+                .Include(i=>i.Images)
+                .Include(i=>i.ItemProperties)
+                .FirstOrDefaultAsync(n=>n.Id== item.Id);
+
+            return itemsDetails;
+        }
+    }
+}
